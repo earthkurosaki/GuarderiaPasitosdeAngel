91779e2b7171482f10a8fa6ebefe9cb0116722a5
Laiyinesuriel <127430023+PapitasLay@users.noreply.github.com>
2024-05-10 10:00:55 -0400

    - Se elimino una carpeta

--- ninos/ninos/conectar_bd.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mysqli= new mysqli("localhost", "root", "12345678", "guarderia1");
-
-if ($mysqli->connect_errno) {
-    echo "No soporto: (". $mysqli->connect_errno . ") " . $mysqli->connect_errno; 
-}; 
-?>
--- ninos/ninos/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once 'conectarBD.php';
-
-$id = $_GET['rn'];
-$query = "DELETE FROM ninos WHERE id = '$id'";
-
-$data = mysqli_query($mysqli, $query);
-header("location: ./index.php?insert=succes");
-?>
