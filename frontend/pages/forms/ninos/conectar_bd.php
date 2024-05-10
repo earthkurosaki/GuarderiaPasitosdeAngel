b829ee64792ae5d18633380a4a40bbb08e5fd05d
Laiyinesuriel <127430023+PapitasLay@users.noreply.github.com>
2024-05-09 22:52:35 -0400

    Cambios al formulario de niños.

--- frontend/pages/forms/ninos/conectar_bd.php
+++ frontend/pages/forms/ninos/conectar_bd.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli= new mysqli("localhost", "root", "", "guarderia");
+$mysqli= new mysqli("localhost", "root", "14160929", "guarderia");
 
 if ($mysqli->connect_errno) {
     echo "No soporto: (". $mysqli->connect_errno . ") " . $mysqli->connect_errno; 
--- frontend/pages/forms/ninos/insertar.php
+++ frontend/pages/forms/ninos/insertar.php
@@ -1,23 +1,26 @@
 <?php
-include 'conectarBD.php'; 
+include 'conectar_bd.php';
 
-$nombre= $_POST['nombre'];
-$fechanac= $_POST['fechanac'];
-$edad= $_POST['edad']; 
-$nacionalidad= $_POST['nacionalidad']; 
-$genero= $_POST['genero']; 
-$modnacer= $_POST['modnacer']; 
-$canthermanos= $_POST['canthermanos']; 
-$alergias= $_POST['alergias']; 
-$especificacion= $_POST['especificacion']; 
-$nivedu= $_POST['nivedu']; 
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$fechanac = $_POST['fechanac'];
+$edad = $_POST['edad'];
+$genero = $_POST['genero'];
+$nacionalidad = $_POST['nacionalidad'];
+$modnacer = $_POST['modnacer'];
+$canthermanos = $_POST['canthermanos'];
+$nivedu = $_POST['nivedu'];
+$alergia = $_POST['alergia'];
+$desc_alergia = $_POST['desc_alergia'];
+$discapacidad = $_POST['discapacidad'];
+$desc_discapacidad = $_POST['desc_discapacidad'];
 
-$sqlinsert = "INSERT into ninos VALUES (0,'$nombre', '$fechanac', '$edad', '$nacionalidad',
-                                          '$genero', '$modnacer', '$canthermanos', '$alergias', 
-                                          '$especificacion', '$nivedu')"; 
+$sqlinsert = "INSERT into nino VALUES (0,'$nombre', '$apellido', '$fechanac', '$edad', '$genero', '$nacionalidad',
+                                         '$modnacer', '$canthermanos', '$nivedu', '$alergia', '$desc_alergia',
+                                         '$discapacidad', '$desc_discapacidad')";
 
-mysqli_query($mysqli, $sqlinsert); 
+mysqli_query($mysqli, $sqlinsert);
 
-header("location:./index.php?insert=success"); 
+header("location:./ninos.php?insert=success");
 
 ?>
