a2b3548d26eb00e34af04a8bf1ea2bd65d85b765
Abbo_boy <125889862+earthkurosaki@users.noreply.github.com>
2024-05-16 08:28:19 -0400

    ultimos cambios con animaciones;

--- frontend/pages/forms/login/conecta.php
+++ frontend/pages/forms/login/conecta.php
@@ -4,4 +4,3 @@ $mysqli= new mysqli("localhost", "root", "Abigail1976.", "guarderia2024");
 if ($mysqli->connect_errno) {
     echo "No soporto: (". $mysqli->connect_errno . ") " . $mysqli->connect_errno; 
 }; 
-?>
